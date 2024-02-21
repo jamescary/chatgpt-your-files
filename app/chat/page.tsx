6726864412014f5eb192c0960c2792ea2a659652
James Cary <eliteeggnog@gmail.com>
2024-02-20 18:18:20 -0600

    swap embedding engine on front-end

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,27 +1,31 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { usePipeline } from '@/lib/hooks/use-pipeline';
-import { cn } from '@/lib/utils';
-import { Database } from '@/supabase/functions/_lib/database';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { useChat } from 'ai/react';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+// import { usePipeline } from '@/lib/hooks/use-pipeline';
+import { OpenAI } from "openai";
+import { cn } from "@/lib/utils";
+import { Database } from "@/supabase/functions/_lib/database";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useChat } from "ai/react";
 
 export default function ChatPage() {
   const supabase = createClientComponentClient<Database>();
 
-  const generateEmbedding = usePipeline(
-    'feature-extraction',
-    'Supabase/gte-small'
-  );
+  // const generateEmbedding = usePipeline(
+  //   "feature-extraction",
+  //   "Supabase/gte-small",
+  // );
+  
+  const openAiKey = `${process.env.OPENAI_API_KEY}`;
+  const openai = new OpenAI({ apiKey: openAiKey });
 
   const { messages, input, handleInputChange, handleSubmit, isLoading } =
     useChat({
       api: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/functions/v1/chat`,
     });
 
-  const isReady = !!generateEmbedding;
+  const isReady = !!openai;
 
   return (
     <div className="max-w-6xl flex flex-col items-center w-full h-full">
@@ -31,8 +35,8 @@ export default function ChatPage() {
             <div
               key={id}
               className={cn(
-                'rounded-xl bg-gray-500 text-white px-4 py-2 max-w-lg',
-                role === 'user' ? 'self-end bg-blue-600' : 'self-start'
+                "rounded-xl bg-gray-500 text-white px-4 py-2 max-w-lg",
+                role === "user" ? "self-end bg-blue-600" : "self-start",
               )}
             >
               {content}
@@ -63,16 +67,17 @@ export default function ChatPage() {
           className="flex items-center space-x-2 gap-2"
           onSubmit={async (e) => {
             e.preventDefault();
-            if (!generateEmbedding) {
-              throw new Error('Unable to generate embeddings');
-            }
+            // if (!generateEmbedding) {
+            //   throw new Error("Unable to generate embeddings");
+            // }
 
-            const output = await generateEmbedding(input, {
-              pooling: 'mean',
-              normalize: true,
+            const {
+              data: [{ embedding }],
+            } = await openai.embeddings.create({
+              model: "text-embedding-3-small",
+              input: input,
+              dimensions: 1024, // Generate an embedding with 1024 dimensions
             });
-
-            const embedding = JSON.stringify(Array.from(output.data));
 
             const {
               data: { session },
@@ -88,7 +93,7 @@ export default function ChatPage() {
                   authorization: `Bearer ${session.access_token}`,
                 },
                 body: {
-                  embedding,
+                  JSON.stringify(Array.from(embedding)),
                 },
               },
             });
